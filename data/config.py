b720b7b572b1b6d762375623b8f5c59afab5dd7c
Aktan08 <40670907+Aktan08@users.noreply.github.com>
2022-03-19 14:51:24 +0300

    Update config.py

--- data/config.py
+++ data/config.py
@@ -2,7 +2,7 @@ BOT_TOKEN = '800754309:AAFc37ouSVuspEFgFXvbkqvIFzY1-d1mzpw'
 
 PROJECT_NAME = '@Aktanchannal_bot'
 
-WEBHOOK_HOST = f"https://arsshopbot.herokuapp.com"
+WEBHOOK_HOST = f"https://emmashop.herokuapp.com"
 WEBHOOK_PATH = '/webhook/' + BOT_TOKEN
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
