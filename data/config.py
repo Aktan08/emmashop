4c4de8dfc4a4ac6496c7709d4cebebef7150d109
Aktan08 <40670907+Aktan08@users.noreply.github.com>
2022-03-19 14:47:21 +0300

    Add files via upload

---
+++ data/config.py
@@ -0,0 +1,9 @@
+BOT_TOKEN = '800754309:AAFc37ouSVuspEFgFXvbkqvIFzY1-d1mzpw'
+
+PROJECT_NAME = '@Aktanchannal_bot'
+
+WEBHOOK_HOST = f"https://arsshopbot.herokuapp.com"
+WEBHOOK_PATH = '/webhook/' + BOT_TOKEN
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+ADMINS = [000000000, 1234567890]
