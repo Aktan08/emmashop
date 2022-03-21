763facdde96ea2e5027c8b1ac4162dd3a37bae53
Aktan08 <Aktan.080800@gmail.com>
2022-03-21 21:28:19 +0300

    Update categories.py

--- keyboards/inline/categories.py
+++ keyboards/inline/categories.py
@@ -12,6 +12,6 @@ def categories_markup():
     markup = InlineKeyboardMarkup()
     for idx, title in db.fetchall('SELECT * FROM categories'):
         markup.add(InlineKeyboardButton(title, callback_data=category_cb.new(id=idx, action='view')))
-    markup.add(InlineKeyboardButton("🛒 Корзина"))
-    # markup.add(InlineKeyboardButton("назад",callback_data= '\menu'))
+    markup.add(InlineKeyboardButton("🛒 Корзина",callback_data= '\menu'))
+    markup.add(InlineKeyboardButton("назад",callback_data= '\menu'))
     return markup
