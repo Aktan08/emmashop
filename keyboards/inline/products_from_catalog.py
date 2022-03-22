9dd6715d1dd40cd4b1d45fd429e074b1723f16d0
Aktan08 <Aktan.080800@gmail.com>
2022-03-22 09:36:53 +0300

    Update products_from_catalog.py

--- keyboards/inline/products_from_catalog.py
+++ keyboards/inline/products_from_catalog.py
@@ -1,7 +1,7 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.callback_data import CallbackData
 from loader import db
-
+from categories import category_cb
 product_cb = CallbackData('product', 'id', 'action')
 
 
@@ -11,5 +11,5 @@ def product_markup(idx='', price=0):
 
     markup = InlineKeyboardMarkup()
     markup.add(InlineKeyboardButton(f'Добавить в корзину - {price}₽', callback_data=product_cb.new(id=idx, action='add')))
-    markup.add(InlineKeyboardButton('<< Назад', callback_data=product_cb.new(id='-', action='list')))
+    markup.add(InlineKeyboardButton('<< Назад', callback_data=category_cb.new(id='-', action='list')))
     return markup
